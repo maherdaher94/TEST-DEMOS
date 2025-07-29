697ad55bb0132524a5715a8efd311ef219c8b00e
maherd94 <maher.daher@adyen.com>
2025-07-29 16:39:23 +0400

    required stuff

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'maher/test-demos',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'reference' => null,
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'adyen/php-api-library' => array(
+            'pretty_version' => 'v27.0.0',
+            'version' => '27.0.0.0',
+            'reference' => '38458a1efccab38a24a77d5954a7ed8788d5993f',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../adyen/php-api-library',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'maher/test-demos' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'reference' => null,
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
